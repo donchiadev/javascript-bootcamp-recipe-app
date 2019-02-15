ee56af9944864ad74796aba6812709ce1cf4e00d
Antonio D'Onchia <antonio.donchia@hotmail.com>
2019-02-15 22:36:27 +0100

    Added some comment into recipes.js

--- src/recipes.js
+++ src/recipes.js
@@ -1,41 +1,54 @@
 import uuidv4 from 'uuid/v4'
 
+// Initializes recipes variable as an empty array into the global space
 let recipes = []
 
+/*
+    This function fetches new value for "recipes" variable 
+    based on the key(omonimus)/value pair stored into the local storage. 
+*/
 const loadRecipes = () => {
      if (localStorage.getItem('recipes')) {
-        return JSON.parse(localStorage.getItem('recipes'))
+        return recipes = JSON.parse(localStorage.getItem('recipes'))
      } else {
-         return []
+         // If no value is provided returns an empty array
+         return recipes = []
      }
 }
 
-const getRecipes = () => {
-    recipes = loadRecipes()
-    return recipes
-}
+// Updates recipes variable and expose it
+const getRecipes = () => loadRecipes()
 
+
+// Sets a new key/value pair into the local storage based on "recipes" variable value
 const saveRecipes = () => {
     localStorage.setItem('recipes', JSON.stringify(recipes))
 }
 
 const addIngredient = (id, ingredientName) => {
+    // Sanitizes ingredientName's string value
     ingredientName = ingredientName.trim().toLowerCase()
+    // Checks recipe presence via id value
     let recipe = recipes.find(recipe => recipe.id === id)
     if (recipe) {
-        const ingredientsArrayIsEmpty = recipe.ingredients.length === 0
         const ingredientIsValid = ingredientName !== ''
         const ingredientNotExist = !Boolean(recipe.ingredients.find(ingredient => ingredient.name === ingredientName))
-        if (ingredientsArrayIsEmpty || (ingredientIsValid && ingredientNotExist) )
-        recipe.ingredients.push({
-            name: ingredientName,
-            inStock: false
-        })
-        saveRecipes()  
+        /*
+            If ingredientName is a valid string and its value 
+            doesn't exists into the recipes array then we push it into the 
+            recipe's ingredients array
+        */
+        if (ingredientIsValid && ingredientNotExist) {
+            recipe.ingredients.push({
+                name: ingredientName,
+                inStock: false
+            })
+            saveRecipes()
+        }
     }
-    // return ingredientName
 }
 
+// Adds a new recipe into the recipes's array
 const createRecipe = title => {
     title = title.trim().toLowerCase()
     let recipe = {
@@ -46,9 +59,9 @@ const createRecipe = title => {
     }
     recipes.push(recipe)
     saveRecipes()
-    // return recipe
 }
 
+// Remove a recipe's ingredient from its ingredients array via ingredient's name
 const removeIngredient = (recipe, ingredientName) => {
     const ingredient = recipe.ingredients.find(ingredient => ingredient.name === ingredientName)
     const ingredientIndex = recipe.ingredients.indexOf(ingredient)
@@ -56,22 +69,23 @@ const removeIngredient = (recipe, ingredientName) => {
     saveRecipes()
 }
 
+// Removes a recipe from recipes array via recipe's id
 const removeRecipe = recipeId => {
     recipes = recipes.filter(recipe => recipe.id !== recipeId)
     saveRecipes()
 }
 
+// Flips recipe ingredient's inStock boolean value
 const toggleInStock = ingredient => {
     ingredient.inStock = !ingredient.inStock
     saveRecipes()
 }
 
+// Changes recipe's steps property value
 const changeSteps = (recipe, steps) => {
     recipe.steps = steps
     saveRecipes()
 }
-
-loadRecipes()
 
 export { 
     getRecipes, 
